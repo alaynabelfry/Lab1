50eec556e0851a0c18a978f2f92df8c79fe7240e
Justin Wilkinson <wilkinj1@algonquincollege.com>
2016-09-19 15:49:20 -0400

    Class work; set up scene graph.

--- SFML-Lab1/main.cpp
+++ SFML-Lab1/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include "TransformNode.h"
 
 int main(int argc, char* argv) 
 {
@@ -20,21 +21,35 @@ int main(int argc, char* argv)
   float growthFactor = 1.001f;
   float shrinkFactor = 0.999f;
 
-  triangle.setOrigin(10, 10);
+  //triangle.setOrigin(10, 10);
+  triangle.setOrigin(20.0f, 20.0f);
   triangle.setPosition(300, 300);
   triangle.setScale(4, 4);
 
   sf::Vector2<float> armPosition((triangle.getLocalBounds().width / 1.75f), (triangle.getLocalBounds().height / 1.5f));
   sf::Vector2<float> armOrigin(0.0f, 2.5f);
 
+  upperArmRect.setOrigin(armOrigin);
+  lowerArmRect.setOrigin(armOrigin);
+
   lowerArmRect.setPosition(15.0f, 2.5f);
   upperArmRect.setPosition(armPosition);
 
+  TransformNode head(&triangle);
+  TransformNode upperArm(&upperArmRect);
+  TransformNode lowerArm(&lowerArmRect);
+
+  upperArm.AddChild(&lowerArm);
+  head.AddChild(&upperArm);
+
+  sf::Clock deltaTime;
   while (window.isOpen())
   {
+    float elaspedTime = deltaTime.restart().asSeconds();
+
     sf::Transform transform = triangle.getTransform();
 
-    triangle.scale(scaleFactor, scaleFactor);
+    /*triangle.scale(scaleFactor, scaleFactor);
     float currentScale = triangle.getScale().x;
     if (currentScale >= 4.0f) {
       scaleFactor = shrinkFactor;
@@ -42,7 +57,7 @@ int main(int argc, char* argv)
     else if (currentScale <= 1.0f)
     {
       scaleFactor = growthFactor;
-    }
+    }*/
 
     sf::Event sfEvent;
     while (window.pollEvent(sfEvent))
@@ -64,14 +79,23 @@ int main(int argc, char* argv)
     if (keyHeld)
     {
       float angleAmount = 90.0f;
-      lowerArmRect.rotate(angleAmount);
-      upperArmRect.rotate(angleAmount);
+      lowerArmRect.rotate(angleAmount * elaspedTime);
+      upperArmRect.rotate(angleAmount * elaspedTime);
     }
 
     window.clear(); // draw fullscreen graphic
+
+    /*sf::Transform hierarchy = sf::Transform::Identity;
+
     window.draw(triangle);
-    window.draw(lowerArmRect); // DRAWING
-    window.draw(upperArmRect); // DRAWING
+    hierarchy = hierarchy * triangle.getTransform();
+
+    window.draw(lowerArmRect, hierarchy); // DRAWING with the triangle's transform.
+    hierarchy = hierarchy * lowerArmRect.getTransform();
+
+    window.draw(upperArmRect, hierarchy); // DRAWING*/
+
+    head.Draw(&window);
 
     window.display();
   }
